e9ce1879cbdf134c44ef7d37fe7dfe826b459549
Derek Doty <ddoty.61414@gmail.com>
2018-08-25 23:47:38 -0400

    updates

--- src/components/Main/Services/Services.js
+++ src/components/Main/Services/Services.js
@@ -8,7 +8,7 @@ function Services(props) {
 			<h2 className='capabilitiesTitle'>Capabilities</h2>
 			<p className='servicesDescription1'>Zephyr Media can film up 6K resolution using CinemaDNG/RAW. All operating pilots are 107 licensed under CFR 14.</p>
 			<br />
-			<p className='servicesDescription2'>Zephyr Media can gain images of real estate where traditional methods are unavailable. In addition, with the advent of drone technology and an experienced pilot allows for large scale real estate evaluation in a fraction of the time.</p>
+			<p className='servicesDescription2'>Zephyr Media can gain images of real estate where traditional methods are unavailable. In addition, the advent of drone technology and an experienced pilot allows for large scale real estate evaluation in a fraction of the time.</p>
 		</div>
 	)
 }
