97e8328e809115292bf8224df479f5862df1ddac
Derek Doty <ddoty.61414@gmail.com>
2018-10-18 21:28:24 -0400

    updates to photo shuffle

--- src/components/Main/ExampleWork/ExampleWork.js
+++ src/components/Main/ExampleWork/ExampleWork.js
@@ -1,106 +1,51 @@
-import React from 'react';
-import ReactPlayer from 'react-player'
+import React from "react";
+import ReactPlayer from "react-player";
 
-import './ExampleWork.css'
+import ExampleSet1 from "./ExampleSet1";
+import ExampleSet2 from "./ExampleSet2";
+import ExampleSet3 from "./ExampleSet3";
+import "./ExampleWork.css";
 
-export default class ExampleWork extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			exampleSet1: '',
-			exampleSet2: '',
-			exampleSet3: '',
-		}
-	}
-
-	render() {
-	const exampleSet1 = [
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephra%20Media%20Photo%201.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%204.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%207.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%2010.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%2013.jpg?raw=true'}
-	]
-
-	const exampleSet2 = [
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%202.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%205.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%208.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%2011.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%2014.jpg?raw=true'}
-	]
-
-	const exampleSet3 = [
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%203.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%206.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%209.jpg?raw=true'},
-		{picture:'https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%2012.jpg?raw=true'}
-	]
-
-	rotateImages = (set) => {
-		this.setState({
-			set: set[0].picture
-		})
-	}
-
-	return (
-		<div className='examples'>
-			<p>Here are some examples of work done</p>
-			<div className='exampleImages'>
-				<img
-					className='image1'
-					alt='First Example'
-					src={rotateImages(exampleSet1)} />
-				{/* <img
-					className='image2' 
-					alt='Second Example'
-					src={cycle(exampleSet2)} />
-				<img
-					className='image3' 
-					alt='Third Example'
-					src={cycle(exampleSet3)} /> */}
-				{/* <img
-					className='image4'
-					alt='Fourth Example'
-					src='https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%2013.jpg?raw=true'/>
-				<img
-					className='image5' 
-					alt='Fifth Example'
-					src='https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%2014.jpg?raw=true'/>
-				<img
-					className='image6' 
-					alt='Sixth Example'
-					src='https://github.com/Ddoty89/Images-for-Zephyr-Media/blob/master/Zephyr%20Media%20Photo%202.jpg?raw=true'/> */}
-			</div>
-			<div className='exampleVideos'>
-            	<   
-	            	ReactPlayer 
-	            	className='exampleVideo1'
-	            	url='https://youtu.be/278Iq24BCwg'
-	            	playing
-	            	autoPlay
-	            	height='250px'
-	            	width='445px'
-	            	loop
-	            	preload='true'
-	            	muted
-	        	/>
-	        	<   
-	            	ReactPlayer 
-	            	className='exampleVideo2'
-	            	url='https://youtu.be/3brA3BHPJDA'
-	            	playing
-	            	autoPlay
-	            	height='250px'
-	            	width='445px'
-	            	loop
-	            	preload='true'
-	            	muted
-	        	/>
-        	</div>
-		</div>
-	)
-	}
-			
-
+function ExampleWork(props) {
+  return (
+    <div className="examples">
+      <div className="exampleSlides">
+        <div className="example1">
+          <ExampleSet1 />
+        </div>
+        <div className="example2">
+          <ExampleSet2 />
+        </div>
+        <div className="example3">
+          <ExampleSet3 />
+        </div>
+      </div>
+      <div className="exampleVideos">
+        <ReactPlayer
+          className="exampleVideo1"
+          url="https://youtu.be/278Iq24BCwg"
+          playing
+          autoPlay
+          height="250px"
+          width="445px"
+          loop
+          preload="true"
+          muted
+        />
+        <ReactPlayer
+          className="exampleVideo2"
+          url="https://youtu.be/3brA3BHPJDA"
+          playing
+          autoPlay
+          height="250px"
+          width="445px"
+          loop
+          preload="true"
+          muted
+        />
+      </div>
+    </div>
+  );
 }
+
+export default ExampleWork;
